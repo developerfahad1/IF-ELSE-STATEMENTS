a03bc5054473a437ad9f0a765b784457ade692e5
developerfahad1 <fahadgraphic.pk>
2024-02-28 22:35:05 -0800

    app.js index.html file created

---
+++ app.js
@@ -0,0 +1,13 @@
+// QNO 1: write a program to check wheather the given input number is divisible by 3 or else show a message number is not divisible by 3 ?
+
+// ANS: 
+var num = 34;
+if(num % 3 === 0){
+    console.log('number is divisible by 3');
+}else{
+    console.log('number is not divisible by 3');
+}
+
+// QNO 2: write a program that check wheather the given input number is an even number or an odd number? 
+
+
