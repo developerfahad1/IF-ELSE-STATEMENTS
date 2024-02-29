e55645d426446c413d1b58306038133518c264b8
developerfahad1 <fahadgraphic.pk>
2024-02-28 23:26:43 -0800

    app.js file changed

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 // QNO 1: write a program to check wheather the given input number is divisible by 3 or else show a message number is not divisible by 3 ?
 
 // ANS: 
-var num = 34;
-if(num % 3 === 0){
-    console.log('number is divisible by 3');
-}else{
-    console.log('number is not divisible by 3');
-}
+// var num = 34;
+// if(num % 3 === 0){
+//     console.log('number is divisible by 3');
+// }else{
+//     console.log('number is not divisible by 3');
+// }
 
 // QNO 2: write a program that check wheather the given input number is an even number or an odd number? 
+
+// ANS:
+// var num = 32;
+// if(num % 2 == 0){
+//  console.log('number is even');
+// }else{
+//     console.log('number is odd');
+// }
+
+QNO:3
+// write an if else statement with the following condition if the varaible age is greather than 18 output "old enough" otherwise output "too young"?
+
+// ANS:
+// var num = 18;
+// if(num > 18){
+//     console.log('old enough');
+// }else{
+//     console.log('too young');
+// }
+
+QNO:4
+// write a program that prompts the user for their name, and then display a special greeting to that person if their name is the same as yours if the name entered by the user is anything other than your name, your code should not produce any output.?
+
+// ANS: 
+
+
+
+// QNO:12
+// This if/else statement does not work. try to fix it: var greeting: var hour = 13; if (hour < 13){greeting ="good day"; else greeting = "good evening";}
+
+ANS:
+// var hour = 13;
+// var greeting;
+
+// if(hour < 13){
+//     greeting = "good day";
+// }else{
+//     greeting = "good evening";
+// }
+
+// console.log(greeting);
+
+
 
 
